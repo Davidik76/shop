888fa337121ba9ad49ae381962dd8204e3bc48fe
Davidik76 <118114060+Davidik76@users.noreply.github.com>
2024-06-22 17:11:49 +0400

    2

--- app.js
+++ app.js
@@ -101,20 +101,20 @@ btn3.addEventListener("click", function(){
 });
 
 
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.Text = "Товар4") {
-		tg.MainButton.hide();	
-	}
-});
-
 // btn4.addEventListener("click", function(){
-// 	if ((tg.MainButton.isVisible) & (tg.MainButton.Text != "Товар4")) {
-// 		tg.MainButton.setText = "Товар4";
-// 	}
-// 	else if (tg.MainButton.Text = "Товар4") {
+// 	if (tg.MainButton.Text = "Товар4") {
 // 		tg.MainButton.hide();	
 // 	}
 // });
+
+btn4.addEventListener("click", function(){
+	if ((tg.MainButton.isVisible) & (tg.MainButton.Text != "Товар4")) {
+		tg.MainButton.setText = "Товар4";
+	}
+	else if (tg.MainButton.Text = "Товар4") {
+		tg.MainButton.hide();	
+	}
+});
 
 // btn5.addEventListener("click", function(){
 // 	if (tg.MainButton.isVisible) {
